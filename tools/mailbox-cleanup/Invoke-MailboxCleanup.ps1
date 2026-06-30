8f80a622f5bb0550357d52939b2df057254c292f
Joshua Garrett <joshua.garrett@corrohealth.com>
2026-06-30 15:40:02 -0400

    fix(mailbox-cleanup): correct archive picker total size and exclude RecoverableItems subfolders from folder list

--- tools/mailbox-cleanup/Invoke-MailboxCleanup.ps1
+++ tools/mailbox-cleanup/Invoke-MailboxCleanup.ps1
@@ -907,8 +907,9 @@ if ($archiveCleanupMode) {
         # Refresh archive folder stats each loop so sizes reflect previous purge
         $allArchiveFolders = Get-MailboxFolderStatistics -Identity $Mailbox -Archive
         $archiveTotalBytes = ($allArchiveFolders |
+            Where-Object { $_.FolderType -eq 'Root' } |
             ForEach-Object { ConvertTo-Bytes $_.FolderAndSubfolderSize } |
-            Measure-Object -Sum).Sum
+            Select-Object -First 1)
         if (-not $archiveTotalBytes) { $archiveTotalBytes = 0 }
         $archiveItemCount = ($allArchiveFolders | Measure-Object -Property ItemsInFolderAndSubfolders -Sum).Sum
         if (-not $archiveItemCount) { $archiveItemCount = 0 }
@@ -923,6 +924,12 @@ if ($archiveCleanupMode) {
             Where-Object {
                 $_.FolderType -ne 'Root' -and
                 $_.FolderType -notlike 'RecoverableItems*' -and
+                $_.FolderPath -notlike '/Recoverable Items*' -and
+                $_.FolderPath -notlike '/DiscoveryHolds*' -and
+                $_.FolderPath -notlike '/Deletions*' -and
+                $_.FolderPath -notlike '/Purges*' -and
+                $_.FolderPath -notlike '/Versions*' -and
+                $_.FolderPath -notlike '/SubstrateHolds*' -and
                 (ConvertTo-Bytes $_.FolderAndSubfolderSize) -gt $PRIMARY_FOLDER_SIZE_THRESHOLD
             } |
             Sort-Object { ConvertTo-Bytes $_.FolderAndSubfolderSize } -Descending
